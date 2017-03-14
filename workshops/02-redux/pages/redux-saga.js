01e51f54b48f8cece24d1e01fd93a8a3341413e7
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-03-14 19:32:48 +0800

    [#2] - Added saga to handle async

--- workshops/02-redux/pages/redux-saga.js
+++ workshops/02-redux/pages/redux-saga.js
@@ -1,16 +1,37 @@
 import { createStore, applyMiddleware } from 'redux'
 import { Provider as Redux } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
-import thunk from 'redux-thunk'
+import { call, take, put, takeLatest } from 'redux-saga/effects'
 
-const initialState = {}
-const reducer = (state = initialState, action) => {
-  return state
+const initialState = {
+  count: 0,
+  failure: false
 }
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'inc':
+      return { ...state, count: state.count + 1 }
+    case 'fail':
+      return { ...state, failure: !state.failure }
+    default:
+      return state
+  }
+}
+
+function* incAction() {
+  return new Promise((res, rej) => {
+    setTimeout(() => res(put({ type: 'inc' })), 1000)
+    rej(put({ type: 'fail' }))
+  })
+}
+function* rootSaga() {
+  yield [
+    takeLatest(incAction),
+  ]
+}
+
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducer, initialState, applyMiddleware(thunk, sagaMiddleware))
-
-
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
 
 sagaMiddleware.run(rootSaga)
 
