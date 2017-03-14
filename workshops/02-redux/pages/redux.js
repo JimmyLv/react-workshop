6568e04b72ac9e93c64a7125132eec10278ee8b1
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-03-14 17:58:28 +0800

    Merge branch 'master' of github.com:LeiZeng/react-workshop
    # Conflicts:
    #	workshops/01-basics/pages/index.js

--- workshops/02-redux/pages/redux.js
+++ workshops/02-redux/pages/redux.js
@@ -1,7 +1,31 @@
-let count = 0
-export default props => (
-  <div>
-    <h1>Redux</h1>
-    <span>Count: {count}</span>
-  </div>
-)
+import { Component } from 'react'
+
+class Redux extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      count: 0
+    }
+    this.timer()
+  }
+
+  timer() {
+    setInterval(
+      () => this.setState({
+        count: this.state.count++
+      }),
+      1000
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Redux</h1>
+        <span>Count: {this.state.count}</span>
+      </div>
+    )
+  }
+}
+
+export default Redux
